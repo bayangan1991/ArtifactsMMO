d7f186abced4c3058927fc6b9720274f8b47cfcd
Ryan Barnes <ryan.barnes@uptickhq.com>
2025-03-25 10:49:01 +1100

    feat: condense characters into single page view

---
+++ frontend/src/components/character-view/character-view.tsx
@@ -0,0 +1,127 @@
+import { faHandFist, faMoon, faTrowel } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+import { useMemo } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+import { useCharacter } from '../../artifactsmmo-client/hooks/use-character.ts'
+import { ActionCard } from '../action-card/action-card.tsx'
+import { ActionQueueCard } from '../action-queue-card/action-queue-card.tsx'
+import { CharacterCard } from '../character-card/character-card.tsx'
+import { InventoryCard } from '../inventory-card/inventory-card.tsx'
+
+import type { FightData, MovementData, RestData, SkillData } from '../../types.ts'
+import { isActionType } from '../../utils/is-action-type.ts'
+
+interface Props {
+  characterName: string
+}
+
+const CharacterView = ({ characterName }: Props) => {
+  const {
+    character,
+    actions: {
+      move,
+      rest,
+      repeatRest,
+      fight,
+      repeatFight,
+      deposit,
+      depositAll,
+      withdraw,
+      gathering,
+      repeatGathering,
+      craft,
+    },
+    lastAction,
+    error,
+    status,
+    timeUntilReady,
+    actionQueue,
+    togglePause,
+  } = useCharacter(characterName || null)
+
+  const lastActionResult: string = useMemo(() => {
+    if (!lastAction) return ''
+    if (isActionType<FightData>(lastAction, 'fight')) {
+      const loot = lastAction.fight.drops.map((item) => `${item.code} x ${item.quantity}`).join(', ')
+      return `${lastAction.fight.result} fight${loot ? `, found: ${loot}` : ''}`
+    }
+    if (isActionType<MovementData>(lastAction, 'movement')) {
+      return `Moving to ${lastAction.destination.name}`
+    }
+    if (isActionType<SkillData>(lastAction, 'gathering')) {
+      return `Gathered ${lastAction.details.items.map((item) => `${item.code} x ${item.quantity}`).join(', ')}`
+    }
+    if (isActionType<RestData>(lastAction, 'rest')) {
+      return `Restored ${lastAction.hp_restored}hp`
+    }
+
+    return lastAction.cooldown.reason
+  }, [lastAction])
+
+  const simpleActions = [
+    {
+      key: 'rest',
+      label: (
+        <>
+          <Icon icon={faMoon} /> Rest
+        </>
+      ),
+      variant: 'success',
+      action: rest,
+      repeat: repeatRest,
+    },
+    {
+      key: 'fight',
+      label: (
+        <>
+          <Icon icon={faHandFist} /> Fight
+        </>
+      ),
+      variant: 'danger',
+      action: fight,
+      repeat: repeatFight,
+    },
+    {
+      key: 'gather',
+      label: (
+        <>
+          <Icon icon={faTrowel} /> Gather
+        </>
+      ),
+      variant: 'warning',
+      action: gathering,
+      repeat: repeatGathering,
+    },
+  ]
+
+  return (
+    <Container fluid>
+      <Row className="g-4">
+        <Col md={6} lg={4} className="d-flex gap-2 flex-column">
+          <CharacterCard
+            character={character}
+            simpleActions={simpleActions}
+            status={status}
+            lastAction={lastActionResult}
+            error={error ?? null}
+            timeUntilReady={timeUntilReady}
+            togglePause={togglePause}
+          />
+          <ActionCard
+            move={move}
+            craft={craft}
+            depositAll={depositAll}
+            currentPosition={character ? { x: character.x, y: character.y } : undefined}
+          />
+          {actionQueue.size() > 0 && <ActionQueueCard queue={actionQueue.data()} handleRemove={actionQueue.remove} />}
+        </Col>
+        <Col className="d-flex gap-2 flex-column">
+          {character && <InventoryCard character={character} depositItem={deposit} withdrawItem={withdraw} />}
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export { CharacterView }
--- frontend/src/pages/character-page/character-page.tsx
+++ frontend/src/pages/character-page/character-page.tsx
@@ -1,123 +1,35 @@
-import { faHandFist, faMoon, faTrowel } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
-import { useMemo } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import { useParams } from 'react-router'
-import { useCharacter } from '../../artifactsmmo-client/hooks/use-character.ts'
-import { ActionCard } from '../../components/action-card/action-card.tsx'
-import { ActionQueueCard } from '../../components/action-queue-card/action-queue-card.tsx'
-import { CharacterCard } from '../../components/character-card/character-card.tsx'
-import { InventoryCard } from '../../components/inventory-card/inventory-card.tsx'
-import type { FightData, MovementData, RestData, SkillData } from '../../types.ts'
-import { isActionType } from '../../utils/is-action-type.ts'
+import {} from '@fortawesome/free-solid-svg-icons'
+import { Card, Nav } from 'react-bootstrap'
+import { Link, useParams } from 'react-router'
+import { useCharacters } from '../../artifactsmmo-client/hooks/use-characters.ts'
+import { CharacterView } from '../../components/character-view/character-view.tsx'
+import type {} from '../../types.ts'
 
 const CharacterPage = () => {
-  const { characterName } = useParams()
-
-  const {
-    character,
-    actions: {
-      move,
-      rest,
-      repeatRest,
-      fight,
-      repeatFight,
-      deposit,
-      depositAll,
-      withdraw,
-      gathering,
-      repeatGathering,
-      craft,
-    },
-    lastAction,
-    error,
-    status,
-    timeUntilReady,
-    actionQueue,
-    togglePause,
-  } = useCharacter(characterName || null)
-
-  const lastActionResult: string = useMemo(() => {
-    if (!lastAction) return ''
-    if (isActionType<FightData>(lastAction, 'fight')) {
-      const loot = lastAction.fight.drops.map((item) => `${item.code} x ${item.quantity}`).join(', ')
-      return `${lastAction.fight.result} fight${loot ? `, found: ${loot}` : ''}`
-    }
-    if (isActionType<MovementData>(lastAction, 'movement')) {
-      return `Moving to ${lastAction.destination.name}`
-    }
-    if (isActionType<SkillData>(lastAction, 'gathering')) {
-      return `Gathered ${lastAction.details.items.map((item) => `${item.code} x ${item.quantity}`).join(', ')}`
-    }
-    if (isActionType<RestData>(lastAction, 'rest')) {
-      return `Restored ${lastAction.hp_restored}hp`
-    }
-
-    return lastAction.cooldown.reason
-  }, [lastAction])
-
-  const simpleActions = [
-    {
-      key: 'rest',
-      label: (
-        <>
-          <Icon icon={faMoon} /> Rest
-        </>
-      ),
-      variant: 'success',
-      action: rest,
-      repeat: repeatRest,
-    },
-    {
-      key: 'fight',
-      label: (
-        <>
-          <Icon icon={faHandFist} /> Fight
-        </>
-      ),
-      variant: 'danger',
-      action: fight,
-      repeat: repeatFight,
-    },
-    {
-      key: 'gather',
-      label: (
-        <>
-          <Icon icon={faTrowel} /> Gather
-        </>
-      ),
-      variant: 'warning',
-      action: gathering,
-      repeat: repeatGathering,
-    },
-  ]
+  const { accountName, characterName } = useParams<{ accountName: string; characterName: string }>()
+  const characters = useCharacters(accountName)
 
   return (
-    <Container fluid>
-      <Row className="g-4">
-        <Col md={6} lg={4} className="d-flex gap-2 flex-column">
-          <CharacterCard
-            character={character}
-            simpleActions={simpleActions}
-            status={status}
-            lastAction={lastActionResult}
-            error={error ?? null}
-            timeUntilReady={timeUntilReady}
-            togglePause={togglePause}
-          />
-          <ActionCard
-            move={move}
-            craft={craft}
-            depositAll={depositAll}
-            currentPosition={character ? { x: character.x, y: character.y } : undefined}
-          />
-          {actionQueue.size() > 0 && <ActionQueueCard queue={actionQueue.data()} handleRemove={actionQueue.remove} />}
-        </Col>
-        <Col className="d-flex gap-2 flex-column">
-          {character && <InventoryCard character={character} depositItem={deposit} withdrawItem={withdraw} />}
-        </Col>
-      </Row>
-    </Container>
+    <Card className="mx-2" style={{ height: 'calc(100vh - 80px)' }}>
+      <Card.Header>
+        <Nav variant="pills" defaultActiveKey={characterName}>
+          {characters.map((char) => (
+            <Nav.Item key={char.name}>
+              <Nav.Link as={Link} to={`/${accountName}/${char.name}/`} eventKey={char.name}>
+                {char.name}
+              </Nav.Link>
+            </Nav.Item>
+          ))}
+        </Nav>
+      </Card.Header>
+      <Card.Body>
+        {characters.map((char) => (
+          <div className={characterName === char.name ? '' : 'd-none'} key={char.name}>
+            <CharacterView characterName={char.name} />
+          </div>
+        ))}
+      </Card.Body>
+    </Card>
   )
 }
 
