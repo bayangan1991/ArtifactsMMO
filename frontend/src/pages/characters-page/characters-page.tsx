1d5a4cfde49971b32a0a10b428a725686ee4a09d
Ryan Barnes <ryan.barnes@uptickhq.com>
2025-03-25 10:17:58 +1100

    fix: character page layout fixes

--- frontend/src/pages/characters-page/characters-page.tsx
+++ frontend/src/pages/characters-page/characters-page.tsx
@@ -8,43 +8,47 @@ const CharactersPage = () => {
   return (
     <Container>
       <Row>
-        <Col
-          style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 200px)', gap: '10px', justifyContent: 'center' }}
-        >
-          {characters.map((character) => (
-            <Card key={character.name} style={{ maxWidth: 200 }}>
-              <Card.Body>
-                <Link to={`${character.name}/`}>
-                  <Card.Img
-                    src={`https://artifactsmmo.com/images/characters/${character.skin}.png`}
-                    height={100}
-                    width={150}
-                    style={{ maxWidth: 80 }}
-                  />
-                </Link>
-                <Card.Title>{character.name}</Card.Title>
-                <Stack gap={2}>
-                  <ProgressBar
-                    variant="danger"
-                    max={character.max_hp}
-                    now={character.hp}
-                    label={`${character.hp}/${character.max_hp}`}
-                  />
-                  <ProgressBar
-                    variant="success"
-                    max={character.max_xp}
-                    now={character.xp}
-                    label={`${character.xp}/${character.max_xp}`}
-                  />
-                  <dl>
-                    <dt>Level</dt>
-                    <dd>{character.level}</dd>
-                  </dl>
-                </Stack>
-              </Card.Body>
-            </Card>
-          ))}
+        <Col lg={2} />
+        <Col lg={8}>
+          <Row className="d-flex">
+            {characters.map((character) => (
+              <Col lg={4} key={character.name} className="p-2 d-flex justify-content-center align-items-center">
+                <Card style={{ maxWidth: 200, width: '100%', margin: 'auto' }}>
+                  <Card.Body>
+                    <Link to={`${character.name}/`}>
+                      <Card.Img
+                        src={`https://artifactsmmo.com/images/characters/${character.skin}.png`}
+                        height={100}
+                        width={150}
+                        style={{ maxWidth: 80 }}
+                      />
+                    </Link>
+                    <Card.Title>{character.name}</Card.Title>
+                    <Stack gap={2}>
+                      <ProgressBar
+                        variant="danger"
+                        max={character.max_hp}
+                        now={character.hp}
+                        label={`${character.hp}/${character.max_hp}`}
+                      />
+                      <ProgressBar
+                        variant="success"
+                        max={character.max_xp}
+                        now={character.xp}
+                        label={`${character.xp}/${character.max_xp}`}
+                      />
+                      <dl>
+                        <dt>Level</dt>
+                        <dd>{character.level}</dd>
+                      </dl>
+                    </Stack>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
         </Col>
+        <Col lg={2} />
       </Row>
     </Container>
   )
