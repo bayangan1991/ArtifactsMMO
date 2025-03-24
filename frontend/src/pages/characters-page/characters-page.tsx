7db4c0abd1129ff689f3bcee39e04acb0724d31e
Ryan Barnes <ryan.barnes@uptickhq.com>
2025-03-24 17:16:25 +1100

    feat: create better character page layout

--- frontend/src/pages/characters-page/characters-page.tsx
+++ frontend/src/pages/characters-page/characters-page.tsx
@@ -1,4 +1,4 @@
-import { Card, Container, ProgressBar, Stack } from 'react-bootstrap'
+import { Card, Col, Container, ProgressBar, Row, Stack } from 'react-bootstrap'
 import { Link, useParams } from 'react-router'
 import { useCharacters } from '../../artifactsmmo-client/hooks/use-characters.ts'
 
@@ -6,39 +6,46 @@ const CharactersPage = () => {
   const { accountName } = useParams()
   const characters = useCharacters(accountName)
   return (
-    <Container className="grid">
-      {characters.map((character) => (
-        <Card key={character.name} style={{ maxWidth: 150 }}>
-          <Card.Body>
-            <Link to={`${character.name}/`}>
-              <Card.Img
-                src={`https://artifactsmmo.com/images/characters/${character.skin}.png`}
-                height={150}
-                width={150}
-              />
-            </Link>
-            <Card.Title>{character.name}</Card.Title>
-            <Stack gap={2}>
-              <ProgressBar
-                variant="danger"
-                max={character.max_hp}
-                now={character.hp}
-                label={`${character.hp}/${character.max_hp}`}
-              />
-              <ProgressBar
-                variant="success"
-                max={character.max_xp}
-                now={character.xp}
-                label={`${character.xp}/${character.max_xp}`}
-              />
-              <dl>
-                <dt>Level</dt>
-                <dd>{character.level}</dd>
-              </dl>
-            </Stack>
-          </Card.Body>
-        </Card>
-      ))}
+    <Container>
+      <Row>
+        <Col
+          style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 200px)', gap: '10px', justifyContent: 'center' }}
+        >
+          {characters.map((character) => (
+            <Card key={character.name} style={{ maxWidth: 200 }}>
+              <Card.Body>
+                <Link to={`${character.name}/`}>
+                  <Card.Img
+                    src={`https://artifactsmmo.com/images/characters/${character.skin}.png`}
+                    height={100}
+                    width={150}
+                    style={{ maxWidth: 80 }}
+                  />
+                </Link>
+                <Card.Title>{character.name}</Card.Title>
+                <Stack gap={2}>
+                  <ProgressBar
+                    variant="danger"
+                    max={character.max_hp}
+                    now={character.hp}
+                    label={`${character.hp}/${character.max_hp}`}
+                  />
+                  <ProgressBar
+                    variant="success"
+                    max={character.max_xp}
+                    now={character.xp}
+                    label={`${character.xp}/${character.max_xp}`}
+                  />
+                  <dl>
+                    <dt>Level</dt>
+                    <dd>{character.level}</dd>
+                  </dl>
+                </Stack>
+              </Card.Body>
+            </Card>
+          ))}
+        </Col>
+      </Row>
     </Container>
   )
 }
