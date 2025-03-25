db50a4f8db9d75b0515b34661df601ebab3e9829
Ryan Barnes <ryan.barnes@uptickhq.com>
2025-03-25 14:12:42 +1100

    refactor: remove card layout and add character to tab

--- frontend/src/pages/character-page/character-page.tsx
+++ frontend/src/pages/character-page/character-page.tsx
@@ -1,5 +1,5 @@
 import {} from '@fortawesome/free-solid-svg-icons'
-import { Card, Nav } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
 import { Link, useParams } from 'react-router'
 import { useCharacters } from '../../artifactsmmo-client/hooks/use-characters.ts'
 import { CharacterView } from '../../components/character-view/character-view.tsx'
@@ -10,26 +10,28 @@ const CharacterPage = () => {
   const characters = useCharacters(accountName)
 
   return (
-    <Card className="mx-2" style={{ height: 'calc(100vh - 80px)' }}>
-      <Card.Header>
-        <Nav variant="pills" defaultActiveKey={characterName}>
-          {characters.map((char) => (
-            <Nav.Item key={char.name}>
-              <Nav.Link as={Link} to={`/${accountName}/${char.name}/`} eventKey={char.name}>
-                {char.name}
-              </Nav.Link>
-            </Nav.Item>
-          ))}
-        </Nav>
-      </Card.Header>
-      <Card.Body>
+    <>
+      <Nav variant="underline" defaultActiveKey={characterName} className="mx-3 mb-3">
         {characters.map((char) => (
-          <div className={characterName === char.name ? '' : 'd-none'} key={char.name}>
-            <CharacterView characterName={char.name} />
-          </div>
+          <Nav.Item key={char.name}>
+            <Nav.Link as={Link} to={`/${accountName}/${char.name}/`} eventKey={char.name}>
+              <img
+                style={{ maxWidth: 15 }}
+                src={`https://www.artifactsmmo.com/images/characters/${char.skin}.png`}
+                className="me-1"
+                alt={char.name}
+              />
+              {char.name}
+            </Nav.Link>
+          </Nav.Item>
         ))}
-      </Card.Body>
-    </Card>
+      </Nav>
+      {characters.map((char) => (
+        <div className={characterName === char.name ? '' : 'd-none'} key={char.name}>
+          <CharacterView characterName={char.name} />
+        </div>
+      ))}
+    </>
   )
 }
 
