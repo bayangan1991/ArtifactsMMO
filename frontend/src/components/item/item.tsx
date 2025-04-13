98055bcd7beecfcd4b72d8f4929e7bc88cbf274d
Ryan Barnes <ryan.bayangan@gmail.com>
2025-04-13 18:29:08 +1000

    fix: add item type to slot fix

--- frontend/src/components/item/item.tsx
+++ frontend/src/components/item/item.tsx
@@ -1,7 +1,7 @@
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 import React, { useContext } from 'react'
-import { Button, Placeholder } from 'react-bootstrap'
+import { Button, Dropdown, Placeholder } from 'react-bootstrap'
 import { Link } from 'react-router'
 import { useItem } from '../../artifactsmmo-client/hooks/use-item'
 import type { components } from '../../artifactsmmo-client/spec'
@@ -14,26 +14,36 @@ interface Props {
   equip?(code: string, slot: components['schemas']['ItemSlot'], quantity?: number): void
 }
 
-const SLOTS = [
-  'weapon',
-  'shield',
-  'helmet',
-  'body_armor',
-  'leg_armor',
-  'boots',
-  'ring1',
-  'ring2',
-  'amulet',
-  'artifact1',
-  'artifact2',
-  'artifact3',
-  'utility1',
-  'utility2',
-  'bag',
-  'rune',
-]
+type SlotItemType =
+  | 'weapon'
+  | 'shield'
+  | 'helmet'
+  | 'body_armor'
+  | 'leg_armor'
+  | 'boots'
+  | 'ring'
+  | 'amulet'
+  | 'artifact'
+  | 'utility'
+  | 'bag'
+  | 'rune'
 
-const isSlotType = (type: string): type is components['schemas']['ItemSlot'] => SLOTS.includes(type)
+const SLOTS: Record<SlotItemType, components['schemas']['ItemSlot'][]> = {
+  weapon: ['weapon'],
+  shield: ['shield'],
+  helmet: ['helmet'],
+  body_armor: ['body_armor'],
+  leg_armor: ['leg_armor'],
+  boots: ['boots'],
+  ring: ['ring1', 'ring2'],
+  amulet: ['amulet'],
+  artifact: ['artifact1', 'artifact2', 'artifact3'],
+  utility: ['utility1', 'utility2'],
+  bag: ['bag'],
+  rune: ['rune'],
+}
+
+const isSlotType = (type: string): type is SlotItemType => Object.keys(SLOTS).includes(type)
 
 const Item = ({ code, imgProps = { height: 25 }, useHistory = false, equip }: Props) => {
   const item = useItem(code)
@@ -60,10 +70,30 @@ const Item = ({ code, imgProps = { height: 25 }, useHistory = false, equip }: Pr
         <img {...imgProps} src={`https://artifactsmmo.com/images/items/${code}.png`} alt={code} />
         <span className="ms-2">{item.name}</span>
       </Link>
-      {equip && isSlotType(itemType) && (
-        <Button className="ms-2" size="sm" variant="outline-light" onClick={() => equip(item.code, itemType, 1)}>
+      {equip && isSlotType(itemType) && SLOTS[itemType].length === 1 && (
+        <Button
+          className="ms-2"
+          size="sm"
+          variant="outline-light"
+          onClick={() => equip(item.code, SLOTS[itemType][0], 1)}
+        >
           <Icon icon={faUser} />
         </Button>
+      )}
+      {equip && isSlotType(itemType) && SLOTS[itemType].length > 1 && (
+        <Dropdown className="d-inline-block">
+          <Dropdown.Toggle className="ms-2" size="sm" variant="outline-light">
+            <Icon icon={faUser} />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {SLOTS[itemType].map((slot) => (
+              <Dropdown.Item key={slot} onClick={() => equip(item.code, slot, 1)}>
+                {slot}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
       )}
     </>
   )
