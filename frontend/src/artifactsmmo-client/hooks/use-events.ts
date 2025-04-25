86fbffb6eb47fd5369817fde451b8e72e425ab7f
Ryan Barnes <ryan.bayangan@gmail.com>
2025-04-25 15:14:14 +1000

    feat: add events hook

---
+++ frontend/src/artifactsmmo-client/hooks/use-events.ts
@@ -0,0 +1,41 @@
+import { useContext, useEffect, useState } from 'react'
+import type { Position } from '../../types.ts'
+import { euclideanDistance } from '../../utils/euclidean-distance.ts'
+import { ApiClientContext } from '../client/context.ts'
+import type { components } from '../spec'
+
+interface Params {
+  currentPosition?: Position
+  page?: number
+}
+
+const useEvents = (params?: Params) => {
+  const { client } = useContext(ApiClientContext)
+  const [events, setEvents] = useState<components['schemas']['DataPage_ActiveEventSchema_'] | null>(null)
+
+  useEffect(() => {
+    client
+      .GET('/events/active', {
+        params: { query: { page: params?.page, size: 100 } },
+      })
+      .then((result) => {
+        if (result.data) setEvents(result.data)
+      })
+      .catch(() => setEvents(null))
+  }, [client, params?.page])
+
+  const { data, ...rest } = events || {}
+
+  if (data && params?.currentPosition) {
+    const currentPosition = params.currentPosition
+    data.sort(
+      (a, b) =>
+        euclideanDistance(currentPosition, { x: a.map.x, y: a.map.y }) -
+        euclideanDistance(currentPosition, { x: b.map.x, y: b.map.y })
+    )
+  }
+
+  return { data, ...rest }
+}
+
+export { useEvents }
